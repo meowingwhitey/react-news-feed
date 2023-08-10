54af9b5198f853f6164c4485e70b498758635631
meowingwhitey <kyp0416@ajou.ac.kr>
2023-08-11 01:43:21 +0900

    EHN: Custom Hook으로 대체 (usePromise)

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
+
 const NewsListBlock = styled.div`
   box-sizing: border-box;
   padding-bottom: 3rem;
@@ -21,30 +22,20 @@ const sampleArticle = {
     urlToImage:'https://via.placeholder.com/160',
 };
 const NewsList = ({category}) => {
-    const [articles, setArticles] = useState(null);
-    const [loading, setLoading] = useState(false);
-
-    useEffect(()=>{
-        const fetchData = async () => {
-            setLoading(true);
-            try{
-                const query = category === 'all' ? '' : `&category=${category}`;
-                const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=9c6a9d6b74604d1ea71f99da31426568${query}`);
-                setArticles(response.data.articles);
-            }
-            catch(e){
-                console.log(e);
-            }
-            setLoading(false);
-        };
-        fetchData();
+    const [loading, response, error] = usePromise(()=>{
+        const query = category === 'all' ? '' : `&category=${category}`;
+        return axios.get(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=9c6a9d6b74604d1ea71f99da31426568${query}`);
     },[category]);
     if(loading){
         return <NewsListBlock>대기 중...</NewsListBlock>
     }
-    if(!articles){
+    if(!response){
         return null;
     }
+    if(error){
+        return <NewsListBlock>에러 발생!</NewsListBlock>
+    }
+    const { articles } = response.data;
     return (
         <div>
             <NewsListBlock>
