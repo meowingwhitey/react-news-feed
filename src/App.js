ba166a9c5a9f7d4bddcf489f24788bfd5e3b2ef5
meowingwhitey <kyp0416@ajou.ac.kr>
2023-08-11 00:12:24 +0900

    ADD: 뉴스 리스트 구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import { useState } from "react";
-import axios from "../node_modules/axios/index";
+import NewsList from "./components/NewsList";
 function App() {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-      try{
-        const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=9c6a9d6b74604d1ea71f99da31426568');
-        setData(response);
-      }
-      catch(e){
-        console.log(e);
-      }
-  };
   return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-    </div>
+    <NewsList/>
   );
 }
 
---
+++ src/components/NewsList.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import axios from 'axios';
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+const sampleArticle = {
+    title:'제목',
+    description:'내용',
+    url:'https://google.com',
+    urlToImage:'https://via.placeholder.com/160',
+};
+const NewsList = () => {
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(()=>{
+        const fetchData = async () => {
+            setLoading(true);
+            try{
+                const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=9c6a9d6b74604d1ea71f99da31426568');
+                setArticles(response.data.articles);
+            }
+            catch(e){
+                console.log(e);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    },[]);
+    if(loading){
+        return <NewsListBlock>대기 중...</NewsListBlock>
+    }
+    if(!articles){
+        return null;
+    }
+    return (
+        <div>
+            <NewsListBlock>
+                {articles.map(article => (
+                    <NewsItem key={article.url} article={article}/>
+                ))}
+            </NewsListBlock>
+        </div>
+    );
+};
+
+export default NewsList;
